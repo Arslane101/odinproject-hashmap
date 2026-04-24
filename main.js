affb9cde627b37f373335808ca3c2c3bd07da604
Arslane101 <rahalabdelhamid@gmail.com>
2026-04-24 11:00:26 +0100

    Refactor changes

--- main.js
+++ main.js
@@ -7,9 +7,12 @@ class Pair{
     }
 }
 class HashMap{
-    load = 0.75
-    size = 16
-    buckets = new Array(this.size)
+    
+    constructor(load,size){
+        this.load = load
+        this.size = size
+        this.buckets = new Array(this.size)
+    }
     
     hash(key){
         let hashcode = 0
@@ -33,8 +36,25 @@ class HashMap{
             return
         }
         else if(this.buckets[hash].key === key) this.buckets[hash].value = value
-        else this.buckets[hash].next = node 
+        else {
+            let temp =  this.buckets[hash] 
+            while( temp.next != null) {
+              temp = temp.next
+              if(temp.key === key) {
+                temp.value = value 
+                return
+            } 
+        }
+         temp.next = node
+
+        if(this.entries().length > this.load * this.size){
+            this.buckets = [...this.buckets, ...new Array(this.size)]
+            this.size = this.buckets.length
+    
+
+        }
           
+    }
     }
     get(key){
         let hash = this.hash(key)
@@ -76,15 +96,35 @@ class HashMap{
      let length = 0
      for(let i=0;i<this.buckets.length;i++){
         let step = this.buckets[i]
+        if(!step) continue
         while(step.next != null){
-            step = step.next
             tmp +=1
+            step = step.next
         }
         length+=tmp
+        console.log(tmp)
         tmp=0
      }
      return length
    }
+   has(key){
+        let hash = this.hash(key)
+        if(hash < 0 || hash >= this.buckets.length){
+            throw new Error("Trying to access index out of bounds")
+
+        }
+        if (!this.buckets[hash]) return false
+        else if(this.buckets[hash].key === key) return true
+        else {
+            let temp = this.buckets[hash]
+            while(temp.key != key && temp.next != null){
+                temp = temp.next
+                console.log("loop")
+            }
+            return true
+        }
+        return false
+    }
    clear(){
      for(let i=0;i<this.buckets.length;i++){
         this.buckets[i]=null
@@ -132,7 +172,7 @@ class HashMap{
      
 }
 
-const test= new HashMap()
+const test= new HashMap(0.75,16)
 test.set('apple', 'red')
 test.set('banana', 'yellow')
 test.set('carrot', 'orange')
@@ -146,8 +186,19 @@ test.set('jacket', 'blue')
 test.set('kite', 'pink')
 test.set('lion', 'golden')
 
+
+
+
+test.set('moon', 'silver')
+
+
+test.set('lion','arslane')
+
+test.set('ice cream','black')
+
+
 console.log(test.entries())
 
-console.log(test.keys())
+console.log(test.length())
 
-console.log(test.values())
+console.log(test.size)
